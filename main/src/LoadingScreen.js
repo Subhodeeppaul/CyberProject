027b23c3ee09b11b0d1b249eb7edc1b4f30dc235
Subhodeep Paul <126082765+Subhodeeppaul@users.noreply.github.com>
2023-06-28 01:48:27 +0530

    loading

--- main/src/LoadingScreen.js
+++ main/src/LoadingScreen.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import './LoadingScreen.css';
 
 const LoadingScreen = () => {
   return (
     <div className="loading-screen">
-      <h1>Loading...</h1>
-      {/* You can add additional loading animations or messages here */}
+        <div className="neon-text">Loading........</div>
+        
+      <div className="container">
+        <div className="cube">
+          <div className="face front"></div>
+          <div className="face back"></div>
+          <div className="face right"></div>
+          <div className="face left"></div>
+          <div className="face top"></div>
+          <div className="face bottom"></div>
+        </div>    
+        </div>
+      <div className="ambient-lighting"></div>
     </div>
+    
   );
 };
 
