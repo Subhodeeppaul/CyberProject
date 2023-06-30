e095c61b2d40a8cc269ba38889ed8cff64e8e65e
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-06-30 12:47:34 +0530

    avf

--- main/src/FirstPage.js
+++ main/src/FirstPage.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './FirstPage.css';
 
 function FirstPage() {
-  function apple() {
-    alert("function called");
-  }
+    function apple() {
+        alert("function called");
+    }
 
-  return (
-    <>
-      <div className="App">
-        <button onClick={apple}>Admin</button>
-      </div>
-      <div className="Ap">
-        <button onClick={apple}>User</button>
-      </div>
-    </>
-  );
+    return ( < >
+        <
+        div class = "container" >
+        <
+        a href = "#" >
+        <
+        span > Admin < /span> <
+        /a> <
+        a href = "#" >
+        <
+        span > User < /span> <
+        /a> <
+        /div> <
+        />
+    );
 }
 
 export default FirstPage;
