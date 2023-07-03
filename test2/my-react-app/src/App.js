100c00dcf54ffc1e31959e907585074b20eb66ce
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-07-04 03:03:30 +0530

    ghh

---
+++ test2/my-react-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+    function apple() {
+        alert("function called");
+    }
+
+
+    return ( < >
+        <
+        div class = "container" >
+        <
+        a href = "#" >
+        <
+        span > Admin < /span>  <
+        /a>  <
+        a href = "#" >
+        <
+        span > User < /span>  <
+        /a>  <
+        /div>  <
+        />
+    );
+}
+
+
+
+export default App;
