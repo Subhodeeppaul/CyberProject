fd7aed921f563b1203e0d619baaf59ed91d86797
Subhodeep Paul <126082765+Subhodeeppaul@users.noreply.github.com>
2023-07-05 01:55:11 +0530

    video

--- test2/my-react-app/src/App.js
+++ test2/my-react-app/src/App.js
@@ -1,42 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const videoStyles = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    zIndex: -1,
+  };
 
-    function apple() {
-        alert("function called");
-    }
-
-
-<<<<<<< HEAD
-    return ( < >
-        <
-        video autoplay loop muted src = "stars.mp4" > < /video>
-
-        <
-        div class = "container" >
-        <
-        a href = "#" >
-        <
-        span > Tickets Booking < /span>  < /
-        a > <
-        a href = "#" >
-        <
-        span > Parking Spot < /span>  < /
-        a > <
-        /div>  < / >
-=======
-    return ( <>
-        <div class = "container" >
-        <
-        a href = "#" >
-        <span > Admin </span>  </a>  <a href = "#" >
-        <span > User </span>  </a>  </div>  
-        </>
->>>>>>> b4729d03277c5083b83bf9e1e4ce103ced3dc614
-    );
+  return (
+    <div className="App">
+      <video autoPlay muted loop style={videoStyles}>
+        <source src={process.env.PUBLIC_URL + '/stars.mp4'} type="video/mp4" />
+      </video>
+      <div className="content">
+        <div className="container">
+          <a href="#">
+            <span>Tickets Booking</span>
+          </a>
+          <a href="#">
+            <span>Parking Spot</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default App;
