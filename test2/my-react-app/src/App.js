aa51ae28edee8d6fefc4c65d4b9aed96235a217a
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-07-06 01:28:14 +0530

    ab

--- test2/my-react-app/src/App.js
+++ test2/my-react-app/src/App.js
@@ -1,33 +1,45 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  const videoStyles = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
-    zIndex: -1,
-  };
+    const videoStyles = {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        zIndex: -1,
+    };
 
-  return (
-    <div className="App">
-      <video autoPlay muted loop style={videoStyles}>
-        <source src={process.env.PUBLIC_URL + '/stars.mp4'} type="video/mp4" />
-      </video>
-      <div className="content">
-        <div className="container">
-          <a href="#">
-            <span>Tickets Booking</span>
-          </a>
-          <a href="#">
-            <span>Parking Spot</span>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+    return ( <
+        >
+        <
+        div className = "content" >
+        <
+        video autoPlay muted loop style = { videoStyles } >
+        <
+        source src = { process.env.PUBLIC_URL + '/stars.mp4' }
+        type = "video/mp4" / >
+        <
+        /video>  <
+        div className = "container" >
+        <
+        a href = "#" >
+        <
+        span > Admin < /span>  <
+        /a>  <
+        a href = "#" >
+        <
+        span > User < /span>  <
+        /a> 
+
+        <
+        /
+        div > <
+        /div>   < / >
+
+    );
 }
 
 export default App;
