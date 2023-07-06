bb6a7d55d0a8c210fe9d8ed82446e861788b3043
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-07-07 01:50:07 +0530

    k

---
+++ test3/my-react-app/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        >
+        <
+        div class = "box" >
+        <
+        h > List of States / Cities < /h> <
+        ul >
+        <
+        li > < span > < /span>West Bengal</li >
+        <
+        li > < span > < /span>Delhi</li >
+        <
+        li > < span > < /span>Maharastra</li >
+        <
+        li > < span > < /span>Tamil Nadu</li >
+        <
+        li > < span > < /span>Karnataka</li >
+        <
+        li > < span > < /span>Madhya Pradesh</li >
+        <
+        li > < span > < /span>Andhra Pradesh</li >
+        <
+        li > < span > < /span>Uttar Pradesh</li >
+        <
+        li > < span > < /span>Jammu and Kashmir</li >
+        <
+        li > < span > < /span>Uttarakhand</li >
+        <
+        li > < span > < /span>Rajasthan</li >
+        <
+        li > < span > < /span>Meghalya</li >
+        <
+        li > < span > < /span>Manipur</li >
+        <
+        li > < span > < /span>Jharkhand</li >
+        <
+        li > < span > < /span>Tripura</li >
+        <
+        /ul> < /
+        div >
+
+        <
+        />
+
+    );
+}
+
+export default App;
