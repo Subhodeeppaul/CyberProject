413f4a6ecc56cf03789cf75bbab7f92d27fd2670
Subhodeep Paul <126082765+Subhodeeppaul@users.noreply.github.com>
2023-07-12 01:07:17 +0530

    c

--- test3/my-react-app/src/App.js
+++ test3/my-react-app/src/App.js
@@ -20,44 +20,30 @@ function App() {
         audio.play();
     }, []);
 
-    return ( <
-        >
-        <
-        div className = "content" >
-        <
-        video autoPlay muted loop style = { videoStyles } >
+    return ( <>
+        <div className = "content" >
+        <video autoPlay muted loop style = { videoStyles } >
         <
         source src = { process.env.PUBLIC_URL + '/bg2.mp4' }
         type = "video/mp4" / >
-        <
-        /video> <
-        div className = "container" >
-        <
-        div className = "box" >
-        <
-        h1 > List of States / Cities < /h1> <
-        ul >
-        <
-        li > < span > < /span><a href>Kolkata</a > < /li > <
-        li > < span > < /span><a href>Delhi</a > < /li > <
-        li > < span > < /span><a href>Maharashtra</a > < /li > <
-        li > < span > < /span><a href>Tamil Nadu</a > < /li > <
-        li > < span > < /span><a href>Karnataka</a > < /li > <
-        li > < span > < /span><a href>Madhya Pradesh</a > < /li > <
-        li > < span > < /span>< a href>Andhra Pradesh</a > < /li > <
-        li > < span > < /span><a href>Uttar Pradesh</a > < /li > <
-        li > < span > < /span><a href>Jammu and Kashmir</a > < /li > <
-        li > < span > < /span><a href>Uttarakhand</a > < /li > <
-        li > < span > < /span><a href>Rajasthan</a > < /li > <
-        li > < span > < /span><a href>Meghalaya</a > < /li > <
-        li > < span > < /span><a href>Manipur</a > < /li > <
-        li > < span > < /span><a href>Jharkhand</a > < /li > <
-        li > < span > < /span><a href>Tripura</a > < /li > <
-        /ul> < /
-        div > <
-        /div> < /
-        div > <
-        />
+        </video> <div className = "container" >
+        <div className = "box" >
+        <h1 > List of States / Cities </h1> <ul >
+        <li > <span > </span><a href>Kolkata</a > </li > 
+        <li > < span > </span><a href>Delhi</a > </li > 
+        <li > < span > </span><a href>Maharashtra</a > </li > <
+        li > < span > </span><a href>Tamil Nadu</a > </li > <
+        li > < span > </span><a href>Karnataka</a > </li > <
+        li > < span > </span><a href>Madhya Pradesh</a > </li > <
+        li > < span > </span>< a href>Andhra Pradesh</a > </li > <
+        li > < span > </span><a href>Uttar Pradesh</a > </li > <
+        li > < span > </span><a href>Jammu and Kashmir</a > </li > <
+        li > < span > </span><a href>Uttarakhand</a > </li > <
+        li > < span > </span><a href>Rajasthan</a > </li > <
+        li > < span > </span><a href>Meghalaya</a > </li > <
+        li > < span > </span><a href>Manipur</a > </li > <
+        li > < span > </span><a href>Jharkhand</a > </li > <
+        li > < span > </span><a href>Tripura</a > </li > </ul> </div > </div> </div > </>
     );
 }
 
