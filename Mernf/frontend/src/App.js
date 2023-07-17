db0c1787a85cb9e6ed840d2a0ae73981ea2abe12
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-07-18 02:48:57 +0530

    nn
    bg

---
+++ Mernf/frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+
+const App = () => (
+  <>
+    <Header />
+    <main style={{ minHeight: "93vh" }}> </main> <Footer />
+  </>
+);
+
+export default App;
