85505ab9a87d7e1cae3c8b95f5fb82949430abe4
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-07-23 02:45:21 +0530

    b

--- Mernf/frontend/src/screens/LandingPage/MyNotes/MyNotes.js
+++ Mernf/frontend/src/screens/LandingPage/MyNotes/MyNotes.js
@@ -1,8 +1,75 @@
+import { Button,Card,Badge, Accordion } from "react-bootstrap";
 import MainScreen from "../../../components/MainScreen";
+import {Link} from "react-router-dom";
+import notes,{} from '../../daata/notes';
 
 const MyNotes = () => {
+  const deleteHandler =(id)=>{
+    if(window.confirm("Are you Sure?")){
+
+    }
+  };
   return (
-    <MainScreen title='Welcome back'>MyNotes</MainScreen>
+    <MainScreen title='Welcome back'>MyNotes
+    <Link to='createnote'>
+      <Button style={{marginLeft:10,marginBottom:6}} size="lg">
+        Create new note
+      </Button>
+      </Link>
+      {
+        notes.map(note =>(
+          <Accordion>
+              <Card style={{margin:10}}>
+                 <Card.Header style={{display:"flex"}}>
+                  <span style={{color:"black",
+                  textDecoration:"none",
+                   flex:1,
+                     cursor:"pointer",
+                      alignSelf:"center",
+                        fontSize:18,}}
+                  
+                  > <Accordion.Toggle as={Card.Text} variant="link" eventKey="0">{note.title}
+
+                  </Accordion.Toggle>
+                    {note.title}
+                  </span>
+                 
+                 <div>
+                  <Button href={'/note/${note._id}'}>Edit</Button>
+                  <Button variant="danger" className="mx-2" onClick={()=>deleteHandler(note._id)}>
+                    Delete</Button>
+                 </div>
+                 </Card.Header>
+                 <Accordion.Collapse eventKey="0">
+                 <Card.Body>
+                  <h4>
+                    <Badge variant="success">
+                     Category-{note.category}
+                    </Badge>
+                  </h4>
+                 <blockquote className="blockquote mb-0">
+          <p>
+            
+           {note.content}
+          </p>
+          <footer className="blockquote-footer">
+            Created on -date
+          </footer>
+        </blockquote>
+                 </Card.Body>
+                 </Accordion.Collapse>
+              
+             </Card>
+          </Accordion>
+         
+        ))
+      }
+             
+            
+    
+    
+    </MainScreen>
+    
   );
 };
 
