339a9f8984f195afd8d963e020df850d8a0ea9ae
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-07-25 01:47:16 +0530

    b

--- Mernf/backend/server.js
+++ Mernf/backend/server.js
@@ -2,17 +2,16 @@ const express = require("express");
 const notes = require("./daata/notes");
 const dotenv = require('dotenv');
 const app = express();
+const connectDB= require("./config/db");
+const userRoutes=require('./routes/userRoutes');
 dotenv.config();
+connectDB();
 app.get('/', (req, res) => {
     res.send("Api runing");
 });
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id);
-
-    res.send(note);
-});
+app.use('/api/users',userRoutes)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Running"));
