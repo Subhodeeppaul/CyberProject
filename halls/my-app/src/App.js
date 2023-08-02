b8742c8c357f70e83a576b7a2d13ee238f86165f
soumyasreepaul <soumyasreepaul2018@gmail.com>
2023-08-02 13:26:50 +0530

    n

---
+++ halls/my-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div class="menu">
+      <ul>
+        <li><a href="#">Kolkata</a></li>
+        <li><a href="#">Delhi</a></li>
+        <li><a href="#">Tamil Nadu</a></li>
+        <li><a href="#">Karnataka</a></li>
+        <li><a href="#">Madhya Pradesh</a></li>
+        <li><a href="#">Andhra Pradesh</a></li>
+        <li><a href="#">Uttar Pradesh</a></li>
+        <li><a href="#">Jammu and Kashmir</a></li>
+        <li><a href="#">Uttarakhand</a></li>
+        <li><a href="#">Rajasthan</a></li>
+        <li><a href="#">Meghalaya</a></li>
+        <li><a href="#">Manipur</a></li>
+        <li><a href="#">Jharkhand</a></li>
+        <li><a href="#">Tripura</a></li>
+        
+        
+      </ul>
+    </div>
+    
+  );
+}
+
+export default App;
